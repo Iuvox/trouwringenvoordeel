3f62c085b024721df7dd62565e7444bedb90ea58
devmerces <79695868+joepbuhre@users.noreply.github.com>
2022-07-21 19:20:52 +0200

    Fixed typing error

--- packages/landingspaginas/src/views/Winactie-Succes.vue
+++ packages/landingspaginas/src/views/Winactie-Succes.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center md:w-2/3 m-auto py-24">
         <h1 class="font-bold text-4xl">Je doet mee!</h1>
-        <p class="font-semibold text-xl">Je doet volledig mee met onze winactie! Aan het einde van elke maand wordt je op de hoogte gebracht of je hebt gewonnen of niet</p>
+        <p class="font-semibold text-xl">Je doet volledig mee met onze winactie! Aan het einde van elke maand word je op de hoogte gebracht of je hebt gewonnen of niet</p>
         <a href="https://trouwringenvoordeel.nl/?utm_source=iuvox_landingspagina&utm_campaign=shoptegoedwinnen&utm_content=succespagina" >
             <BaseButton class="md:w-1/3 mt-4">Bekijk onze collectie</BaseButton>
         </a>
